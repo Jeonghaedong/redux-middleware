5fd70348f0b86afcaa21461cc3a1d02f428c288c
jeong <abit0718@naver.com>
2021-08-17 20:45:50 +0900

    리덕스 미들웨어준비

---
+++ src/modules/counter.js
@@ -0,0 +1,21 @@
+// action type
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+// action 생성함수
+
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+const initialState = 0;
+
+export default function counter(state= initialState, action ) {
+    switch (action.type) {
+        case INCREASE :
+            return state +1;
+        case DECREASE :
+            return state -1;
+        default:
+            return state;
+    }
+}
