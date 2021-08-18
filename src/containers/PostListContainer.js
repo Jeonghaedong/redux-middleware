5ef7adace17cfe3b00c6eb6aec1d63c0ae82aedc
jeong <abit0718@naver.com>
2021-08-18 13:38:56 +0900

    라우트설정하기

--- src/containers/PostListContainer.js
+++ src/containers/PostListContainer.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import PostList from '../components/PostList';
-import { getPosts } from '../modules/posts';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import PostList from "../components/PostList";
+import { getPosts } from "../modules/posts";
 
 function PostListContainer() {
   const { data, loading, error } = useSelector(state => state.posts.posts);
@@ -15,6 +15,7 @@ function PostListContainer() {
   if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러 발생!</div>;
   if (!data) return null;
+
   return <PostList posts={data} />;
 }
 
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,21 +1,21 @@
-import * as postsAPI from '../api/posts'; // api/posts 안의 함수 모두 불러오기
+import * as postsAPI from "../api/posts"; // api/posts 안의 함수 모두 불러오기
 import {
   createPromiseThunk,
   reducerUtils,
   handleAsyncActions
-} from '../lib/asyncUtils';
+} from "../lib/asyncUtils";
 
 /* 액션 타입 */
 
 // 포스트 여러개 조회하기
-const GET_POSTS = 'GET_POSTS'; // 요청 시작
-const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'; // 요청 성공
-const GET_POSTS_ERROR = 'GET_POSTS_ERROR'; // 요청 실패
+const GET_POSTS = "GET_POSTS"; // 요청 시작
+const GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS"; // 요청 성공
+const GET_POSTS_ERROR = "GET_POSTS_ERROR"; // 요청 실패
 
 // 포스트 하나 조회하기
-const GET_POST = 'GET_POST';
-const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
-const GET_POST_ERROR = 'GET_POST_ERROR';
+const GET_POST = "GET_POST";
+const GET_POST_SUCCESS = "GET_POST_SUCCESS";
+const GET_POST_ERROR = "GET_POST_ERROR";
 
 // 아주 쉽게 thunk 함수를 만들 수 있게 되었습니다.
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
@@ -32,11 +32,11 @@ export default function posts(state = initialState, action) {
     case GET_POSTS:
     case GET_POSTS_SUCCESS:
     case GET_POSTS_ERROR:
-      return handleAsyncActions(GET_POSTS, 'posts')(state, action);
+      return handleAsyncActions(GET_POSTS, "posts")(state, action);
     case GET_POST:
     case GET_POST_SUCCESS:
     case GET_POST_ERROR:
-      return handleAsyncActions(GET_POST, 'post')(state, action);
+      return handleAsyncActions(GET_POST, "post")(state, action);
     default:
       return state;
   }
